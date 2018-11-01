5c6b493e0905e814752543aebc86f8538082a95f
Osman Hussein <osman_990@hotmail.com>
2018-11-01 13:30:51 +0000

    Created two rows of movies which is a table, used flex to remove the padding but not confident on flex at the moment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MovieImage from './image.jpg';
+import BluePlanet from './tv-movie-logos/blue_planet.jpg';
+import PrisonBreak from './tv-movie-logos/prison_break.jpg';
+import OldMovie from './tv-movie-logos/old_movie.jpg'
 
 class App extends Component {
   render() {
@@ -14,7 +16,23 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <img class="movie-image-1" src={MovieImage} alt="logo"/>
+
+        <table className="movies-table">
+          <tr className="row">
+            <img className="movie" src={BluePlanet} alt="movie1"/>
+            <img className="movie" src={PrisonBreak} alt="movie1"/>
+            <img className="movie" src={OldMovie} alt="movie1"/>
+          </tr>
+          <tr className="row">
+            <img className="movie" src={OldMovie} alt="movie1"/>
+            <img className="movie" src={BluePlanet} alt="movie1"/>
+            <img className="movie" src={PrisonBreak} alt="movie1"/>
+          </tr>
+        </table>
+        {/* <img class="movie" src="https://jonman492000.files.wordpress.com/2014/02/sw_epit5-026.jpg" alt="logo"/>
+        <img className="movie" src="https://upload.wikimedia.org/wikipedia/en/thumb/8/84/Prison_Break_%28miniseries%29.jpg/220px-Prison_Break_%28miniseries%29.jpg" alt="movie1"/>
+        <img className="movie" src="https://m.media-amazon.com/images/M/MV5BNjI1M2ZjMzItZWI4Ny00ZWJlLWI0ZDAtMTJhNDQxOWZjM2M5XkEyXkFqcGdeQXVyMjExMjk0ODk@._V1_.jpg" alt="movie1"/>
+        <img className="movie" src="https://i.ytimg.com/vi/3VjP9-Dw-Rs/maxresdefault.jpg" alt="movie1"/> */}
       </div>
     );
   }
