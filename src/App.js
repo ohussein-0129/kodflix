070d8865ea008dd3fb6f6e460f98d2a8d2b48ff5
Osman Hussein <osman_990@hotmail.com>
2018-11-11 18:59:03 +0000

    stated to add designs for tablets

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
+          {/* <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" /> 
             <h1 className="App-title">Welcome to React</h1>
           </header>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          </p> */}
               <Switch>
                 <Route exact path='/'  component={AllTVShows}/>
                 <Route path='/:movie' component={MovieDetails} />
