e8e9761cec38c2187cc513f704ff4df502276b3e
Osman Hussein <osman_990@hotmail.com>
2018-11-12 09:43:26 +0000

    removed the default react header and paragraph

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MovieDetails from './components/movie-details/MovieDetails';
@@ -11,13 +10,6 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" /> 
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p> */}
               <Switch>
                 <Route exact path='/'  component={AllTVShows}/>
                 <Route path='/:movie' component={MovieDetails} />
