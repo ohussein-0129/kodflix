45c9a104083d375a60f1b94eb3c190d7cd1ae86d
Osman Hussein <osman_990@hotmail.com>
2018-11-13 09:36:55 +0000

    moved the array to a new file also the images have been moved

---
+++ src/components/tv-shows/gallery/GalleryGet.js
@@ -0,0 +1,35 @@
+import {Arrow, PrisonBreak, Dexter, HateChris, Hawaii_5_0, TheWestWing} from '../images/covers/ImageLinks';
+
+const tvShowCovers = [
+    {
+        name: 'Arrow',
+        id: 1,
+        imagePath: Arrow
+    },
+    {
+        name: 'Everybody Hates Chris',
+        id: 2,
+        imagePath: HateChris
+    },
+    {
+        name: 'Pirson Break',
+        id: 3,
+        imagePath: PrisonBreak
+    },
+    {
+        name: 'Hawaii Five-0',
+        id: 4,
+        imagePath: Hawaii_5_0
+    },
+    {
+        name: 'Dexter',
+        id: 5,
+        imagePath: Dexter
+    },
+    {
+        name: 'The West Wing',
+        id: 6,
+        imagePath: TheWestWing
+    },
+];
+export default tvShowCovers;
