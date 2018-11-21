8471bc8405ba91a7340b72457647703364f0101f
Osman Hussein <osman_990@hotmail.com>
2018-11-21 14:35:58 +0000

    connected the backend with the frontend

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,19 +3,22 @@ const app = express();
 // const fs = require("fs");
 const tvShows = require("./ShowsList");
 const path = require('path');
-const port = process.env.PORT || 3001;
-
-
-app.get('/main', function (req, res) {
-    res.sendFile(path.join(__dirname,  '../../public', 'index.html'));
-});
-
-app.get('/', (req, res) => res.send('Hello World!'));
+//const port = process.env.PORT || 5000;
+const port = 3001;
 
 app.get('/rest/show', (req, res)=>{
     res.type('json');
     res.end(JSON.stringify(tvShows.tvShows));
 });
+
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname,  '../../build', 'index.html'));
+});
+
+//app.get('/', (req, res) => res.send('Hello World!'));
+
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
