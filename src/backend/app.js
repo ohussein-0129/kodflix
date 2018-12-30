bf5e81827c84cd471deba7ba4770f04ad434b873
Osman Hussein <osman_990@hotmail.com>
2018-12-30 12:55:02 +0000

    removed the show list as data is coming from mongodb

--- src/backend/app.js
+++ src/backend/app.js
@@ -43,4 +43,12 @@ app.get('/images/:id', function (req, res) {
         res.sendFile(path.join(__dirname,  './image/covers', `${req.params.id}.jpg`));
     }
 });
+
+//link to the index.html
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname,  '../../build', 'index.html'));
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
